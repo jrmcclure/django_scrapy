f3a8956fd80a646eacda49db02bfb588a5c88a69
mcscruf61 <mcclurejr@gmail.com>
2017-08-24 15:51:30 -0600

    Updated spider & models

--- dj_scrapy/app/admin.py
+++ dj_scrapy/app/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from app.models import TeamScore
 # Register your models here.
 
-admin.site.register(TeamScore)
+
+class TeamScoreAdmin(admin.ModelAdmin):
+    list_display = ('abbv', 'time', 'week',
+                    'tmTotalPts', 'oppTotalPts', 'oppId')
+    list_filter = ('abbv', 'week', 'oppId')
+
+admin.site.register(TeamScore, TeamScoreAdmin)
--- dj_scrapy/app/models.py
+++ dj_scrapy/app/models.py
@@ -4,17 +4,24 @@ from django.db import models
 
 
 class TeamScore(models.Model):
-    name = models.CharField(max_length=255)
-    abbv = models.CharField(max_length=16, db_index=True)
     teamId = models.SmallIntegerField(db_index=True)
+    abbv = models.CharField(max_length=16, db_index=True)
+    name = models.CharField(max_length=255)
     oppId = models.SmallIntegerField(db_index=True)
+    oppAbbv = models.CharField(max_length=16, db_index=True)
+    oppName = models.CharField(max_length=255)
     time = models.DateTimeField(null=True)
     week = models.SmallIntegerField(db_index=True)
-    tmTotalPts = models.FloatField(null=True, default=0.0)
-    team_ytp = models.SmallIntegerField(null=True)
-    team_ip = models.SmallIntegerField(null=True)
-    team_pmr = models.SmallIntegerField(null=True)
-    team_liveproj = models.FloatField(null=True, default=0.0)
+    tmTotalPts = models.DecimalField(default=0.0, decimal_places=1, max_digits=4)
+    oppTotalPts = models.DecimalField(default=0.0, decimal_places=1, max_digits=4)
+    team_pts_diff = models.DecimalField(default=0.0, decimal_places=1, max_digits=4)
+    team_ytp = models.SmallIntegerField(default=0)
+    team_ip = models.SmallIntegerField(default=0)
+    team_pmr = models.SmallIntegerField(default=0)
+    team_liveproj = models.DecimalField(default=0.0, decimal_places=1, max_digits=4)
+    opp_liveproj = models.DecimalField(default=0.0, decimal_places=1, max_digits=4)
+    team_proj_diff = models.DecimalField(default=0.0, decimal_places=1, max_digits=4)
+    final = models.BooleanField(default=False)
 
     def __str__(self):
         return self.abbv
